f870ac1daa0517db58a2e2c5e9576cbe3a16aafe
praveenui298 <praveent3373@gmail.com>
2023-11-07 11:48:06 +0530

    destructuring added for useState hook

--- REACT/app-functions/src/hooks/01.UseState.js
+++ REACT/app-functions/src/hooks/01.UseState.js
@@ -20,3 +20,44 @@ const UseState = () => {
 }
 
 export default UseState;
+
+
+//Object Destructuring
+// we can have alias in OD, i.e username as profileName
+let user = {
+    username: 'Praveen',
+    age: 29,
+    getName(){
+        return 5
+    }
+}
+let {username:profileName, age, getName} = user;
+console.log(profileName, age, getName());
+
+// Array Destructuring
+// we can't have alias in AD
+
+/* in function */
+function useState(){
+    let user2 = {
+        username1: 'Praveen',
+        age1: 29,
+        getName1(){
+            return 5
+        }
+    }
+    return user2
+}
+let [username1, age1, getName1] = useState();
+console.log(username1, age1, getName());
+
+/*
+let user2 = {
+    username1: 'Praveen',
+    age1: 29,
+    getName1(){
+        return 5
+    }
+}
+let [username1, age1, getName1] = user2;
+*/
